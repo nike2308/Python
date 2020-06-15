9c01e7e4c69bc571e2f55c90b11a828e43618c73
nike2308 <nikitapaskov@mail.ru>
2020-06-15 22:51:33 +0300

    123

---
+++ Practick/backup_ver2.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+source = ['"C:\\My Documents"', 'C:\\Code']
+target_dir = 'E:\\Backup' 
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+if not os.path.exists(today):
+    os.mkdir(today) 
+    print('Каталог успешно создан', today)
+target = today + os.sep + now + '.zip'
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+if os.system(zip_command) == 0:
+    print('Резервная копия успешно создана в', target)
+else:
+    print('Создание резервной копии НЕ УДАЛОСЬ')
---
+++ using_dict.py
@@ -0,0 +1,19 @@
+ab = {  'Swaroop'   : 'swaroop@swaroopch.com',
+        'Larry'     : 'larry@wall.org',
+        'Matsumoto' : 'matz@ruby-lang.org',
+        'Spammer'   : 'spammer@hotmail.com'
+     }
+
+print("Адрес Swaroop'а:", ab['Swaroop'])
+
+del ab['Spammer']
+
+print('\nВ адресной книге {0} контакта\n'.format(len(ab)))
+
+for name, address in ab.items():
+    print('Контакт {0} с адресом {1}'.format(name, address))
+
+ab['Guido'] = 'guido@python.org'
+
+if 'Guido' in ab:
+    print("\nАдрес Guido:", ab['Guido'])
